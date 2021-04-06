21967a74b380ea3572658d72f96db91089df27a4
Nico Braun <nico-braun@live.de>
2021-04-06 09:32:27 +0000

    invoke python

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+exec("python -V", (error, stdout, stderr) => {
+  if (error) {
+    core.setFailed(error.message);
+    return;
+  }
+  if (stderr) {
+    core.setFailed(stderr);
+    return;
+  }
+  return core.getInput(`stdout: ${stdout}`);
+});
